7a2565339ba551838f765e20b76a70efe48e8e6f
Lukaspds91 <lucas.p.silva1991@gmail.com>
2022-05-09 13:47:45 -0300

    BackendSTS

---
+++ ProjetoFarmacia/src/main/java/com/generation/projetofarmacia/model/Categoria.java
@@ -0,0 +1,74 @@
+package com.generation.projetofarmacia.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo tipo é obrigatório e não pode conter espaços em branco")
+	@Size(min = 3, max = 50, message = "O atributo tipo deve conter no mínimo 3 e no máximo 50 caracteres")
+	private String tipo;
+	
+	@NotNull(message = "O atributo palavra chave é Obrigatório!")
+	@Size(min = 3, max = 15, message = "O atributo palavra chave deve conter no mínimo 3 e no máximo 15 caracteres")
+	private String palavraChave;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}	
+
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getPalavraChave() {
+		return palavraChave;
+	}
+
+	public void setPalavraChave(String palavraChave) {
+		this.palavraChave = palavraChave;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
+	
+}
